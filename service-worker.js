4999aac0fb13fb80109ad206a82598787f214ee4
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2019-10-03 16:04:14 +0800

    10/3/2019 - master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.060edb13d659883d360e9e8992cb8160.js"
+  "precache-manifest.ce26458b352de4fb79d714ad30866992.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app"});
